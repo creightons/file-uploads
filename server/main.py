dd8fe2ed0d57d294f0126a44124fbcdef5121678
creightons <creightons26@gmail.com>
2017-07-25 23:43:32 -0400

    Added file uploading based off a flask tutorial, still needs some work

--- server/main.py
+++ server/main.py
@@ -2,4 +2,9 @@ from flask import Flask
 
 app = Flask(__name__)
 
+# upload path is relative to the directory the app starts in
+UPLOAD_FOLDER = './uploads'
+
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 app.config['DEBUG'] = 1
+app.secret_key = 'test key'
--- server/routes.py
+++ server/routes.py
@@ -1,7 +1,47 @@
-from flask import render_template
+import os
+from flask import render_template, request, redirect, flash, url_for, send_from_directory
+from werkzeug.utils import secure_filename
+
+
+ALLOWED_EXTENSIONS = frozenset(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
+
+def allowed_file(filename):
+    return '.' in filename and \
+        filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+def get_path(app, filename):
+    directory_path = os.path.abspath(app.config['UPLOAD_FOLDER'])
+    filepath = os.path.join(directory_path, filename)
+    return filepath
 
 def apply_routes(app):
 
-    @app.route('/')
+    @app.route('/', methods = ['GET', 'POST'])
     def index():
+        if request.method == 'POST':
+            if 'file' not in request.files:
+                flash('No file part')
+                return redirect(request.url)
+
+            file = request.files['file']
+
+            if file.filename == '':
+                flash('No selected file')
+                return redirect(request.url)
+
+            if file and allowed_file(file.filename):
+                filename = secure_filename(file.filename)
+                filepath = get_path(app, filename)
+
+                file.save(filepath)
+
+                return redirect(url_for('uploaded_file', filename=filename))
+            else:
+                flash('failed to upload file')
+                
+
         return render_template('index.html')
+
+    @app.route('/uploads/<filename>', methods = ['GET', 'POST'])
+    def uploaded_file(filename):
+        return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
